9acf43604857f003f1da0fa738bd1cdc527ccb76
mamahda <gbrnmhd89@gmail.com>
2026-03-23 01:31:04 +0700

    migrasi framework dari gin ke http only

---
+++ src/app/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+func main() {
+	serverTime := time.Now()
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		// TODO: Bikin Cors GET Method Only
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET")
+
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		currentTime := time.Now()
+
+		// TODO: Bikin response buat endpoint /health
+		response := map[string]interface{}{
+			"nama":      "Gilbran Mahdavikia Raja",
+			"nrp":       "5025241134",
+			"status":    "UP",
+			"timestamp": currentTime,
+			"uptime":    serverTime,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(response)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
